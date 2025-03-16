a827fd2972c6fbc99b0e1270f51cc123cf6e7913
Nimish Singh <nimishsingh@Nimishs-MacBook-Air.local>
2025-03-16 15:36:36 +0530

    Replaces map with TokenBucket object to track last refill time and remaining tokens

--- src/main/java/ratelimiter/TokenBucketRateLimiter.java
+++ src/main/java/ratelimiter/TokenBucketRateLimiter.java
@@ -4,11 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TokenBucketRateLimiter implements RateLimiter {
-    // customer -> last refill time -> remaining tokens map
-    /*
-        Such a mapping is required to make it work correctly for rate limites for different users
-     */
-    private final Map<Integer, Map<Integer, Integer>> customerTokenBuckets;
+    private final Map<Integer, TokenBucket> customerTokenBuckets;
     // Maximum tokens the bucket can hold
     private final int maxTokens;
     // Tokens added per second
@@ -27,13 +23,13 @@ public class TokenBucketRateLimiter implements RateLimiter {
     public boolean shouldAllowRequest(int customerId, int timestamp) {
         refillTokens(customerId, timestamp);
 
-        Map<Integer, Integer> lastRefillTimeTokensMap = customerTokenBuckets.get(customerId);
+        TokenBucket bucket = customerTokenBuckets.get(customerId);
 
-        Integer lastRefillTimestamp = lastRefillTimeTokensMap.keySet().stream().findFirst().orElse(timestamp);
-        int currentTokens = lastRefillTimeTokensMap.get(lastRefillTimestamp);
+        int lastRefillTimestamp = bucket.getLastRefillTime();
+        int currentTokens = bucket.getRemainingTokens();
 
         if (currentTokens > 0) {
-            lastRefillTimeTokensMap.put(lastRefillTimestamp, currentTokens - 1);
+            customerTokenBuckets.put(customerId, new TokenBucket(lastRefillTimestamp, currentTokens - 1));
             return true;
         }
 
@@ -42,22 +38,39 @@ public class TokenBucketRateLimiter implements RateLimiter {
 
     private void refillTokens(int customerId, int timestamp) {
         if (!customerTokenBuckets.containsKey(customerId)) {
-            Map<Integer, Integer> lastRefillTimeTokensMap = new HashMap<>();
-            lastRefillTimeTokensMap.put(timestamp, maxTokens);
-            customerTokenBuckets.put(customerId, lastRefillTimeTokensMap);
+            TokenBucket bucket = new TokenBucket(timestamp, maxTokens);
+            customerTokenBuckets.put(customerId, bucket);
             return;
         }
 
-        Map<Integer, Integer> lastRefillTimeTokensMap = customerTokenBuckets.get(customerId);
-        int lastRefillTime = lastRefillTimeTokensMap.keySet().stream().findFirst().get();
-        int currentTokens = lastRefillTimeTokensMap.get(lastRefillTime);
+        TokenBucket bucket = customerTokenBuckets.get(customerId);
+        int lastRefillTime = bucket.getLastRefillTime();
+        int currentTokens = bucket.getRemainingTokens();
         int tokensToRefill = ((timestamp - lastRefillTime) / refillWindow) * refillRate;
 
         if (tokensToRefill > 0) {
             int tokensToReset = Math.min(currentTokens + tokensToRefill, maxTokens);
-            // Effectively keeping this map as a single-entry map
-            lastRefillTimeTokensMap.remove(lastRefillTime);
-            lastRefillTimeTokensMap.put(timestamp, tokensToReset);
+            TokenBucket newBucket = new TokenBucket(timestamp, tokensToReset);
+            customerTokenBuckets.put(customerId, newBucket);
         }
+    }
+}
+
+// Is thread-safe because of immutability
+class TokenBucket {
+    private final int lastRefillTime;
+    private final int remainingTokens;
+
+    public TokenBucket(int lastRefillTime, int remainingTokens) {
+        this.lastRefillTime = lastRefillTime;
+        this.remainingTokens = remainingTokens;
+    }
+
+    public int getLastRefillTime() {
+        return lastRefillTime;
+    }
+
+    public int getRemainingTokens() {
+        return remainingTokens;
     }
 }
