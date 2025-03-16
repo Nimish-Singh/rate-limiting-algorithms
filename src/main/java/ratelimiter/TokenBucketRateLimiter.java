5dff9e2adf31caa554290f29dcc3618738adf519
Nimish Singh <nimishsingh@Nimishs-MacBook-Air.local>
2025-03-16 14:42:50 +0530

    Adds basic implementation for token bucket algorithm

---
+++ src/main/java/ratelimiter/TokenBucketRateLimiter.java
@@ -0,0 +1,59 @@
+package ratelimiter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokenBucketRateLimiter implements RateLimiter {
+    // customer -> last refill time -> remaining tokens map
+    /*
+        Such a mapping is required to make it work correctly for rate limites for different users
+     */
+    private final Map<Integer, Map<Integer, Integer>> customerTokenBuckets;
+    // Maximum tokens the bucket can hold
+    private final int maxTokens;
+    // Tokens added per second
+    private final int refillRate;
+
+    public TokenBucketRateLimiter(int maxTokens, int refillRate) {
+        this.maxTokens = maxTokens;
+        this.refillRate = refillRate;
+        this.customerTokenBuckets = new HashMap<>();
+    }
+
+    @Override
+    public boolean shouldAllowRequest(int customerId, int timestamp) {
+        refillTokens(customerId, timestamp);
+
+        Map<Integer, Integer> lastRefillTimeTokensMap = customerTokenBuckets.get(customerId);
+
+        Integer lastRefillTimestamp = lastRefillTimeTokensMap.keySet().stream().findFirst().orElse(timestamp);
+        int currentTokens = lastRefillTimeTokensMap.get(lastRefillTimestamp);
+
+        if (currentTokens > 0) {
+            lastRefillTimeTokensMap.put(lastRefillTimestamp, currentTokens - 1);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void refillTokens(int customerId, int timestamp) {
+        if (!customerTokenBuckets.containsKey(customerId)) {
+            Map<Integer, Integer> lastRefillTimeTokensMap = new HashMap<>();
+            lastRefillTimeTokensMap.put(timestamp, maxTokens);
+            customerTokenBuckets.put(customerId, lastRefillTimeTokensMap);
+            return;
+        }
+
+        Map<Integer, Integer> lastRefillTimeTokensMap = customerTokenBuckets.get(customerId);
+        int lastRefillTime = lastRefillTimeTokensMap.keySet().stream().findFirst().get();
+        int currentTokens = lastRefillTimeTokensMap.get(lastRefillTime);
+        int tokensToRefill = (timestamp - lastRefillTime) * refillRate;
+
+        if (tokensToRefill > 0) {
+            int tokensToReset = Math.min(currentTokens + tokensToRefill, maxTokens);
+            lastRefillTimeTokensMap.clear();
+            lastRefillTimeTokensMap.put(timestamp, tokensToReset);
+        }
+    }
+}
---
+++ src/test/java/ratelimiter/TokenBucketRateLimiterTest.java
@@ -0,0 +1,55 @@
+package ratelimiter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TokenBucketRateLimiterTest {
+    private final TokenBucketRateLimiter rateLimiter = new TokenBucketRateLimiter(5, 2);
+
+    @Test
+    public void shouldAllowRequestWhenMultipleCustomerRequestsIntertwine() {
+        int aCustomerId = 100;
+        int anotherCustomerId = 200;
+
+        assertTrue(rateLimiter.shouldAllowRequest(aCustomerId, 11));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 11));
+
+        assertTrue(rateLimiter.shouldAllowRequest(aCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+
+        assertTrue(rateLimiter.shouldAllowRequest(aCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+
+        assertTrue(rateLimiter.shouldAllowRequest(aCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+
+        assertTrue(rateLimiter.shouldAllowRequest(aCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+
+        assertTrue(rateLimiter.shouldAllowRequest(aCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+
+        assertFalse(rateLimiter.shouldAllowRequest(aCustomerId, 12));
+        assertFalse(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+
+        assertTrue(rateLimiter.shouldAllowRequest(aCustomerId, 13));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 13));
+    }
+
+    @Test
+    public void shouldAllowRequestWhenMultipleCustomerRequestsDoNotIntertwine() {
+        int aCustomerId = 100;
+        int anotherCustomerId = 200;
+
+        assertTrue(rateLimiter.shouldAllowRequest(aCustomerId, 11));
+
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+        assertTrue(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+        assertFalse(rateLimiter.shouldAllowRequest(anotherCustomerId, 12));
+    }
+}
