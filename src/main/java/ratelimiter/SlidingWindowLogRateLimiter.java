18cf94e0b337b014bd97047de249ed8ee988b0bd
Nimish Singh <nimishsingh@Nimishs-MacBook-Air.local>
2025-03-20 00:56:57 +0530

    Makes sliding window log rate limiter thread safe

--- src/main/java/ratelimiter/SlidingWindowLogRateLimiter.java
+++ src/main/java/ratelimiter/SlidingWindowLogRateLimiter.java
@@ -1,36 +1,28 @@
 package ratelimiter;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
 
 public class SlidingWindowLogRateLimiter implements RateLimiter {
     /*
         To further optimise on storage, we can use TreeMap instead of Queue, with mapping between timestamp and count of requests at that timestamp- this prevents us from storing each separate timestamp.
         The time complexity will still be O(windowSize) in both.
      */
-    private final Map<Integer, Queue<Integer>> customerRequestTimestampsMap;
+    private final ConcurrentMap<Integer, ConcurrentLinkedQueue<Integer>> customerRequestTimestampsMap;
     private final int windowSize;
     private final int maxAllowedRequests;
 
     public SlidingWindowLogRateLimiter(int maxAllowedRequests, int windowSize) {
         this.maxAllowedRequests = maxAllowedRequests;
         this.windowSize = windowSize;
-        this.customerRequestTimestampsMap = new HashMap<>();
+        this.customerRequestTimestampsMap = new ConcurrentHashMap<>();
     }
 
     @Override
     public boolean shouldAllowRequest(int customerId, int timestamp) {
-        if (!customerRequestTimestampsMap.containsKey(customerId)) {
-            Queue<Integer> requestTimestamps = new LinkedList<>();
-            requestTimestamps.offer(timestamp);
-
-            customerRequestTimestampsMap.put(customerId, requestTimestamps);
-            return true;
-        }
-
-        Queue<Integer> requestTimestamps = customerRequestTimestampsMap.get(customerId);
+        // computeIfAbsent() is thread-safe
+        ConcurrentLinkedQueue<Integer> requestTimestamps = customerRequestTimestampsMap.computeIfAbsent(customerId, k -> new ConcurrentLinkedQueue<>());
 
         while (!requestTimestamps.isEmpty() && requestTimestamps.peek() < timestamp - windowSize)
             requestTimestamps.poll();
